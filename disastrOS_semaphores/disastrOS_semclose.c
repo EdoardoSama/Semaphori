65ee83460deeba04d4458732f9c158446957ada6
EdoardoSama <spinetti.1758549@studenti.uniroma1.it>
2019-07-22 16:17:43 +0200

    Cose4

--- disastrOS_semaphores/disastrOS_semclose.c
+++ disastrOS_semaphores/disastrOS_semclose.c
@@ -5,28 +5,36 @@
 #include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
+#include "disastrOS_constants.h"
+#include "disastrOS_descriptor.h"
+#include "disastrOS_globals.h"
 
 void internal_semClose(){
   //1 retrieve the fd of the resource to close
   int fd=running->syscall_args[0];
 
-  SemDescriptor* des=SemDescriptorList_byFd(&running->sem_descriptors, fd);
-  //2 if the fd is not in the the process, we return an error
-  if (! des){
-    running->syscall_retvalue=-1;
-    return;
+
+  Semaphore* res = SemaphoreList_byId(&(semaphores_list), fd);
+  if(!res){
+    printf("Errore: semaforo non in lista\n");
+		running->syscall_retvalue=-1;
+		return;
   }
 
-  //3 we remove the SemDescriptor from the process list
-  des = (SemDescriptor*) List_detach(&running->sem_descriptors, (ListItem*) des);
-  assert(des);
-
-  Semaphore* res=des->ptr;
-
-  // we remove the SemDescriptor pointer from the resource list
-  SemDescriptorPtr* desptr=(SemDescriptorPtr*) List_detach(&res->descriptors, (ListItem*)(des->ptr));
-  assert(desptr);
-  SemDescriptor_free(des);
-  SemDescriptorPtr_free(desptr);
-  running->syscall_retvalue=0;
-}
+  SemDescriptor* semdes = (SemDescriptor*)running->sem_descriptors.first;
+  if(!semdes){
+    printf("Errore: descrittore non trovato\n");
+		running->syscall_retvalue=-1;
+		return;
+  }
+  while(semdes != NULL){
+    SemDescriptorPtr* semptr = (SemDescriptorPtr*) semdes->ptr;
+    if(!semptr){
+      printf("Errore: puntatore a descrittore non trovato\n");
+      running->syscall_retvalue=-1;
+      return;
+      }
+      List_detach(semptr, (ListItem*)semptr->descriptor);
+      semdes->list.next;
+    } 
+  }
