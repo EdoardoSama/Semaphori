bb151bc53202d54886f6f44c22d5083c191e0a88
EdoardoSama <spinetti.1758549@studenti.uniroma1.it>
2019-07-22 14:56:10 +0200

    Cose3

--- disastrOS_semaphores/disastrOS_semclose.c
+++ disastrOS_semaphores/disastrOS_semclose.c
@@ -7,5 +7,26 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+  //1 retrieve the fd of the resource to close
+  int fd=running->syscall_args[0];
+
+  SemDescriptor* des=SemDescriptorList_byFd(&running->sem_descriptors, fd);
+  //2 if the fd is not in the the process, we return an error
+  if (! des){
+    running->syscall_retvalue=-1;
+    return;
+  }
+
+  //3 we remove the SemDescriptor from the process list
+  des = (SemDescriptor*) List_detach(&running->sem_descriptors, (ListItem*) des);
+  assert(des);
+
+  Semaphore* res=des->ptr;
+
+  // we remove the SemDescriptor pointer from the resource list
+  SemDescriptorPtr* desptr=(SemDescriptorPtr*) List_detach(&res->descriptors, (ListItem*)(des->ptr));
+  assert(desptr);
+  SemDescriptor_free(des);
+  SemDescriptorPtr_free(desptr);
+  running->syscall_retvalue=0;
 }
